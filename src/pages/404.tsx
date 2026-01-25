5451a99ea300b03c15de5c08521ea09bd85df38a
Miguel Diaz <miguel.diaz@kimera.dev>
2026-01-25 19:35:45 +0100

    Update navigation and documentation structure: rename 'Tutorial' to 'Getting Started', add new categories for User Guide, Integrator Guide, Architecture, API Reference, SDK Reference, Tutorials, FAQ, and Changelog. Introduce custom 404 page with a link to Getting Started.

---
+++ src/pages/404.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode } from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import Heading from '@theme/Heading';
+
+export default function NotFound(): ReactNode {
+  return (
+    <Layout title="Page Not Found">
+      <div className="container margin-vert--xl">
+        <div className="row">
+          <div className="col col--6 col--offset-3">
+            <Heading as="h1" className="hero__title">
+              404 - Page Not Found
+            </Heading>
+            <p>We could not find what you were looking for.</p>
+            <p>
+              <Link to="/docs/getting-started/overview">
+                Return to Getting Started
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/getting-started/overview"
           >
-            Docusaurus Tutorial
+            Get Started
           </Link>
         </div>
       </div>
